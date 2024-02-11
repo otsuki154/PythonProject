e07d7f25ba2348b9655a7f6503f62be72c482637
otsuki154 <nvthanh15490@gmail.com>
2024-02-11 10:46:24 +0900

    change server batch

--- HelloPython/Library/Mail/GetNewsVnExpressServer.py
+++ HelloPython/Library/Mail/GetNewsVnExpressServer.py
@@ -8,28 +8,29 @@ from unidecode import unidecode
 from datetime import datetime
 from PIL import Image
 from io import BytesIO
+
 def init():
     #Link các mục bài báo
     urls = [
             "https://vnexpress.net/the-thao",
-            "https://vnexpress.net/kinh-doanh",
-            "https://vnexpress.net/thoi-su/chinh-tri",
-            "https://vnexpress.net/khoa-hoc",
-            "https://vnexpress.net/the-gioi",
-            "https://vnexpress.net/giao-duc",
-            "https://vnexpress.net/giai-tri",
-            "https://vnexpress.net/suc-khoe",
+            # "https://vnexpress.net/kinh-doanh",
+            # "https://vnexpress.net/thoi-su/chinh-tri",
+            # "https://vnexpress.net/khoa-hoc",
+            # "https://vnexpress.net/the-gioi",
+            # "https://vnexpress.net/giao-duc",
+            # "https://vnexpress.net/giai-tri",
+            # "https://vnexpress.net/suc-khoe",
             ]
     #categoryId tương ứng với các mục bài báo
     categoryIds = [
                     3,
-                    4,
-                    5,
-                    6,
-                    7,
-                    8,
-                    9,
-                    10,
+                    # 4,
+                    # 5,
+                    # 6,
+                    # 7,
+                    # 8,
+                    # 9,
+                    # 10,
                    ]
     combined_dict = dict(zip(categoryIds, urls))
     # Thư mục để lưu trữ hình ảnh
@@ -40,9 +41,9 @@ def init():
         user="postgres",
         password="postgres",
         host="localhost",
-        port="5432"
+        port="5433"
     )
-    numArticles = 5
+    numArticles = 10
     return combined_dict, image_folder, conn, numArticles
 def convert_to_formatted_time(date_str):
     # Tách chuỗi theo dấu phẩy
@@ -103,6 +104,44 @@ def get_article_content(url):
     else:
         print(f"Failed to fetch content. Status code: {response.status_code}")
     return None
+
+def download_image1(image_folder,image_url, article_title):
+    # Tạo thư mục nếu chưa tồn tại
+    os.makedirs(image_folder, exist_ok=True)
+    if image_url:
+        # Tạo đường dẫn đầy đủ cho hình ảnh
+        full_image_path = os.path.join(image_folder, f"{article_title}.jpg")
+
+        # Tải hình ảnh và lưu vào thư mục images
+        image_data = requests.get(image_url).content
+        with open(full_image_path, 'wb') as image_file:
+            image_file.write(image_data)
+
+def download_image2(image_folder, image_url, article_title, target_size_kb=80):
+    # Tạo thư mục nếu chưa tồn tại
+    os.makedirs(image_folder, exist_ok=True)
+    if image_url:
+        # Tạo đường dẫn đầy đủ cho hình ảnh
+        full_image_path = os.path.join(image_folder, f"{article_title}.jpg")
+
+        # Tải hình ảnh và giảm kích thước về 80KB
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            image = Image.open(BytesIO(response.content))
+
+            # Lấy tỉ lệ giảm kích thước để đạt được dung lượng mong muốn
+            target_size_bytes = target_size_kb * 1024
+            current_size_bytes = len(response.content)
+            if current_size_bytes > target_size_bytes:
+                # Resize ảnh
+                new_width = 800
+                new_height = int((image.height/image.width) * new_width)
+                resized_image = image.resize((new_width, new_height))
+            else:
+                resized_image = image
+
+            # Lưu lại ảnh đã được resize
+            resized_image.save(full_image_path, 'JPEG', quality=85)
 
 def download_image(image_folder, image_url, article_title, target_size_kb=80):
     # Tạo thư mục nếu chưa tồn tại
