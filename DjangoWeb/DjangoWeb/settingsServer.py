df5ef84abc0ed9e70f145e9c5166722e0ed49d8d
otsuki154 <nvthanh15490@gmail.com>
2024-02-13 10:57:23 +0900

    add setting server

--- DjangoWeb/DjangoWeb/settingsServer.py
+++ DjangoWeb/DjangoWeb/settingsServer.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jp6juhssco7w(q)#8!(h3&9j1(*o!_+a8c93)^d3d7o(rvd2q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.0.228', 'news.nvthanh.online','bao.nvthanh.online','tintuc.nvthanh.online']
+ALLOWED_HOSTS = ['127.0.0.1','192.168.0.228', 'news.nvthanh.online','nvthanh.online','tintuc.nvthanh.online']
 
 
 # Application definition
